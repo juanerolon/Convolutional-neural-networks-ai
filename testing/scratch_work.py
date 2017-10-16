525a9bcc3e90b053b5056b0262d15c758883ad9b
Enrique Rolon <rolon.math@gmail.com>
2017-10-16 18:54:13 -0500

    updates

--- testing/scratch_work.py
+++ testing/scratch_work.py
@@ -76,3 +76,12 @@ cv_rgb = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
 # display the image, along with bounding box
 plt.imshow(cv_rgb)
 plt.show()
+
+#
+# returns "True" if face is detected in image stored at img_path
+#########################################################################
+def face_detector(img_path):
+    img = cv2.imread(img_path)
+    gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+    faces = face_cascade.detectMultiScale(gray)
+    return len(faces) > 0
